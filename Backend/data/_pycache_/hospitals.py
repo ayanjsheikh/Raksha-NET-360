2a807c4e81a996e59e03a75a85a947ad55ce3306
tanishkajain2030-source <tanishkajain2030@gmail.com>
2026-07-31 09:58:59 +0530

    Add files via upload

---
+++ Backend/data/_pycache_/hospitals.py
@@ -0,0 +1,30 @@
+hospitals = [
+    {
+        "id": 1,
+        "name": "AIIMS Delhi",
+        "latitude": 28.5672,
+        "longitude": 77.2100,
+        "phone": "01126588500"
+    },
+    {
+        "id": 2,
+        "name": "Safdarjung Hospital",
+        "latitude": 28.5690,
+        "longitude": 77.2005,
+        "phone": "01126730000"
+    },
+    {
+        "id": 3,
+        "name": "Civil Hospital Gurgaon",
+        "latitude": 28.4595,
+        "longitude": 77.0266,
+        "phone": "01242320000"
+    },
+    {
+        "id": 4,
+        "name": "Apollo Hospital",
+        "latitude": 28.5408,
+        "longitude": 77.2836,
+        "phone": "01171791090"
+    }
+]
